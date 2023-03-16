814d041e5010d090beb5964559b6c6be048c5669
shehharyar <sheharyarhassan1456@gmail.com>
2023-03-16 09:59:05 +0500

    created array using class

--- Data_Structures/Arrays/main.js
+++ Data_Structures/Arrays/main.js
@@ -1,5 +1,39 @@
-var array =[ 1, 2, 3, 5, 5, 6,]
+class Array {
+    constructor(){
+        this.length= 0;
+        this.data= {};
+    }
+    get(index){
+        return this.data[index];
+    }
+    push(item){
+        this.data[this.length]= item;
+        this.length++;
+        return this.length;
+    }
+    pop(){
+        const lastItem= this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+    delete(index){
+        const item = this.data[index];
+        this.shiftItem(index);
+    }
+    shiftItem(index){
+        for (let i = index; i < this.length-1; i++){
+            this.data[i]= this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+    }
+}
 
-f0r(var  i in  array ){
-    console.log(i)
-}  
+const newArray= new Array();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!');
+newArray.delete(0);
+
+console.log(newArray);
