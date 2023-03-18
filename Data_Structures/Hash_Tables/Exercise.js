e0015387e3afdc909765c37af5338a0dfdd0e7d8
shehharyar <sheharyarhassan1456@gmail.com>
2023-03-19 01:14:01 +0500

    solved exercise

---
+++ Data_Structures/Hash_Tables/Exercise.js
@@ -0,0 +1,33 @@
+// finding recurrent numbers in array
+
+// Given an array= [2, 1, 3, 2,4,5 , 6,]
+// it should return 1
+
+
+function firstRecurringCharacter(input){
+    for(let i = 0; i < input.length; i++){
+        for(let j = i+ 1; j < input.length; j++ ){
+            if(input[i] === input[j]){
+                return input[i];
+            }
+        }
+    }
+    return undefined;
+}// o(n ^ 2)
+
+function firstRecurringCharacter2(input){
+    let map={};
+    for(let i = 0; i < input.length; i++){
+        if(map[input[i]] !== undefined){
+            return input[i]
+        }
+        else{
+            map[input[i]]=i
+        }
+        console.log(map);
+    }
+    return undefined;
+}
+
+
+console.log(firstRecurringCharacter2([2, 1,3, 4, 2,3,3, ]))
